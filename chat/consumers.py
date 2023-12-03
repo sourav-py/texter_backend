82ee339eed476ba87aca6e0a1967675054464746
Sourav Singh <imsrv2k@gmail.com>
2023-12-03 14:23:14 +0530

    Fetching user id from ws connection scope

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,15 +1,22 @@
 import json
+import jwt
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+
+from .models import Message
 
 
 class ChatConsumer(WebsocketConsumer):
     def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
-        print(self.scope)
-        # Join room group
+
+        #Fetching user id from ws scope 
+        jwtToken = self.scope['cookies'].get('jwt')
+        payload = jwt.decode(jwtToken,'secret',algorithms=['HS256'])
+        userId = payload['id']
+
         async_to_sync(self.channel_layer.group_add)(
             self.room_group_name, self.channel_name
         )
@@ -26,6 +33,14 @@ class ChatConsumer(WebsocketConsumer):
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+
+        #Fetching user id from ws scope 
+        jwtToken = self.scope['cookies'].get('jwt')
+        payload = jwt.decode(jwtToken,'secret',algorithms=['HS256'])
+        userId = payload['id']
+
+        messageObj = Message.objects.create(sender=userId,chatroom=int(self.room_name),body=message)
+        messageObj.save()
 
         # Send message to room group
         async_to_sync(self.channel_layer.group_send)(
