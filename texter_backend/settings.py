65c3860b07226b039eb85a37f0e5521907fd94b6
Sourav Singh <imsrv2k@gmail.com>
2024-01-20 12:10:53 +0530

    deployment: Added vercel as allowed hosts

--- texter_backend/settings.py
+++ texter_backend/settings.py
@@ -73,7 +73,7 @@ MIDDLEWARE = [
 ]
 
 CORS_ORIGIN_ALLOW_ALL = True
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1','localhost', '.vercel.app']
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOW_METHODS = [
